db974e8c6be8b32f2663246af8fe996dba4345f1
Imran Ahmed <imran.nappa@gmail.com>
2013-09-09 15:31:38 +1200

    Update config.js
    vagrant modification

--- config.js
+++ config.js
@@ -17,8 +17,7 @@ var config = new Settings(
   // By default this will attempt to reach ES at the same host you have
   // elasticsearch installed on. You probably want to set it to the FQDN of your
   // elasticsearch host
-  elasticsearch:    "http://"+window.location.hostname+":9200",
-  // elasticsearch: 'http://localhost:9200',
+  elasticsearch:    "http://33.33.33.10:9200",
   kibana_index:     "kibana-int",
   modules:          ['histogram','map','pie','table','filtering',
                     'timepicker','text','fields','hits','dashcontrol',
